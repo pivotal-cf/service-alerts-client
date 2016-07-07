3584323d4aef1232c82c3bd0f70010e6e144118d
Josh Hill <jhill@pivotal.io>
2016-07-07 18:02:38 +0100

    It's not a release, it's a perfectly normal binary
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- ci/build-binary.sh
+++ ci/build-binary.sh
@@ -13,8 +13,8 @@ must_have() {
 
 IMPORT_PATH=github.com/pivotal-cf/service-alerts-client
 CLIENT_DIR=$PWD/src/$IMPORT_PATH
-RELEASE_VERSION="$(client_git tag --list 'v*' --contains HEAD | sed -n 's/^v//p')"
-must_have RELEASE_VERSION
+BINARY_VERSION="$(client_git tag --list 'v*' --contains HEAD | sed -n 's/^v//p')"
+must_have BINARY_VERSION
 
 export GOPATH=$PWD
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o bin/service-alerts-client-$RELEASE_VERSION $IMPORT_PATH/cmd/send-service-alert
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o bin/service-alerts-client-$BINARY_VERSION $IMPORT_PATH/cmd/send-service-alert
